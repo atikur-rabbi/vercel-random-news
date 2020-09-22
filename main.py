a7c17ef2020b55d142e75380457690d07089c95f
atikur-rabbi <msawon94@gmail.com>
2020-09-22 16:53:02 +0600

    Updated at 2020-09-22_0453 PM

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ app = Flask(__name__)
 @app.route("/",methods=['GET', 'POST'])
 def home():
 	import requests,random,re
-	main_list={"all":"https://inshorts.vercel.app/all","national":"https://inshorts.vercel.app/national",
-	           "buisness":"https://inshorts.vercel.app/business","sports":"https://inshorts.vercel.app/sports",
-	           "world":"https://inshorts.vercel.app/world","politics":"https://inshorts.vercel.app/politics",
-	           "technology":"https://inshorts.vercel.app/technology","startup":"https://inshorts.vercel.app/startup",
-	           "entertainment":"https://inshorts.vercel.app/entertainment",
-	           "science":"https://inshorts.vercel.app/science","automobile":"https://inshorts.vercel.app/automobile"}
+	main_list={"all":"https://api.atik.cf/inshorts/all",
+				"national":"https://api.atik.cf/inshorts/national",
+				"business":"https://api.atik.cf/inshorts/business",
+				"sports":"https://api.atik.cf/inshorts/sports",
+				"world":"https://api.atik.cf/inshorts/world",
+				"politics":"https://api.atik.cf/inshorts/politics",
+				"technology":"https://api.atik.cf/inshorts/technology",
+				"startup":"https://api.atik.cf/inshorts/startup",
+				"entertainment":"https://api.atik.cf/inshorts/entertainment",
+				"science":"https://api.atik.cf/inshorts/science",
+				"automobile":"https://api.atik.cf/inshorts/automobile"}
 	tag=['all', 'national', 'buisness', 'sports', 'world', 'politics', 'technology', 'startup', 'entertainment',
 	     'science', 'automobile']
 	main_list_selection=random.choice([i for i in range(0,11)])
@@ -18,7 +23,7 @@ def home():
 	data=requests.get(url).json()
 	news_selection=random.choice([i for i in range(0,len(data['articles']))])
 	news=data['articles'][news_selection]
-	news['title']=re.sub(' +', ' ', news['title'])[1:-1:1]
+	# news['title']=re.sub(' +', ' ', news['title'])[1:-1:1]
 	news['tag']=tag[main_list_selection]
 	return render_template('index.html',news=news)
 
